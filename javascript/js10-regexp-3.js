185d122fec2c630fcb313c2227e968f4f9e74167
Sérgio Almeida <almeida.sergiomc@gmail.com>
2025-07-08 08:44:19 +0100

    Added solution for js10-regexp-3.js

---
+++ javascript/js10-regexp-3.js
@@ -0,0 +1,11 @@
+// URL: https://www.hackerrank.com/challenges/js10-regexp-3/
+
+function regexVar() {
+    /*
+     * Declare a RegExp object variable named 're'
+     * It must match ALL occurrences of numbers in a string.
+     */
+    const re = /[+-]?\d+/g
+    
+    return re;
+}
